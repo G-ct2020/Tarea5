03c37636dafbc181dccb6c33f8738cbc76ec34c0
G-ct2020 <81890604+G-ct2020@users.noreply.github.com>
2021-06-24 23:12:14 -0400

    Add files via upload

---
+++ NombreApellido.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NombreApellido
+{
+    public static void main(String []NombreApellido)
+    {Scanner identificador=new Scanner(System.in);
+        String n="",a="";
+        System.out.println("Ingrese un nombre");
+        n=identificador.nextLine();
+        System.out.println("Ingrese un apellido");
+        a=identificador.nextLine();
+        System.out.println("Hola mi nombre es: "+n);
+          System.out.println("Mi apellido es: "+a);
+    }
+}
